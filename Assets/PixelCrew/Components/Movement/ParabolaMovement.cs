1cdbe97e5ea2493cf68736c231e4060df2b68ccd
Shainur <disbat2017@mail.ru>
2021-12-20 18:20:33 +0300

    ParabolaMovent added

---
+++ Assets/PixelCrew/Components/Movement/ParabolaMovement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Components.Movement
+{
+    public class ParabolaMovement : MonoBehaviour
+    {
+        public static Vector2 ParabolaMove(Vector2 start, Vector2 end, float height, float t)
+        {
+            Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+            var mid = Vector2.Lerp(start, end, t);
+            return new Vector2(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t));
+        }
+    }
+}
