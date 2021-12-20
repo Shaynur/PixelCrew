088dafc280dc4dccfea9f3d04beb57ebbb20d394
Shainur <disbat2017@mail.ru>
2021-12-20 17:46:41 +0300

    ShowTarget Camera

---
+++ Assets/PixelCrew/Camera/CameraStateController.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Camera
+{
+    public class CameraStateController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        private static readonly int ShowTargetKey = Animator.StringToHash("ShowTarget");
+
+        public void SetPosition(Vector3 targetPosition)
+        {
+            targetPosition.z = _camera.transform.position.z;
+            _camera.transform.position = targetPosition;
+        }
+
+        public void SetState(bool state)
+        {
+            _animator.SetBool(ShowTargetKey, state);
+        }
+    }
+}
